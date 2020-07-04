def31fb77c298326bf79043441d5e2478e80fe21
yuandunbin <yuandb@qianbaocard.com>
2020-07-04 23:48:30 +0800

    添加test模块

---
+++ app/src/main/java/com/test/Oom.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.ArrayList;
+
+/**
+ * @author yuandunbin
+ * @date 2020/3/11
+ */
+public class Oom {
+    public static void main(String[] args){
+        ArrayList<Object> objects = new ArrayList<>();
+    }
+}
